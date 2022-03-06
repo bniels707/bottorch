ff7bd16ca7accff00445b643b0043c01410e96bf
Brandon Nielsen <nielsenb@jetfuse.net>
2022-03-05 19:04:22 -0600

    Initial commit of an example that seems to halfway work.

---
+++ botdata.py
@@ -0,0 +1,104 @@
+import torch
+
+import pandas as pd
+
+from functools import partial
+from torch.utils.data import Dataset
+from torchvision.transforms import Lambda
+
+NAME_1_COL_IDX = 0 #Bot 1 Name
+WEAPON_1_1_COL_IDX = 1 #Bot 1, Weapon 1
+WEAPON_1_2_COL_IDX = 2 #Bot 1, Weapon 2
+
+NAME_2_COL_IDX = 3 #Bot 2 Name
+WEAPON_2_1_COL_IDX = 4 #Bot 2, Weapon 1
+WEAPON_2_2_COL_IDX = 5 #Bot 2, Weapon 2
+
+NAME_WINNER_IDX = 6 #Winner bot name
+
+class BotDataset(Dataset):
+    def __init__(self, input_file, transform=None, target_transform=None):
+        self.input_data = pd.read_csv(input_file, header=None)
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        #Shape is rows x cols
+        return self.input_data.shape[0]
+
+    def __getitem__(self, idx):
+        data_row = self.input_data.iloc[idx]
+
+        data = data_row[0:NAME_WINNER_IDX]
+        label = data_row[NAME_WINNER_IDX]
+
+        if self.transform:
+            data = self.transform(data)
+        if self.target_transform:
+            label = self.target_transform(label)
+
+        return data, label
+
+def _get_botname_lambda(botnames):
+    #Returns a lambda for projecting robot names as a one-hot tensor
+    #https://datascience.stackexchange.com/questions/30215/what-is-one-hot-encoding-in-tensorflow
+    return Lambda(lambda y: torch.zeros(len(botnames), dtype=torch.float).scatter_(dim=0, index=torch.tensor(botnames.index(y)), value=1))
+
+def _get_weapon_lambda(weapons):
+    #Returns a lambda for projecting weapon as a one-hot tensor
+    return Lambda(lambda y: torch.zeros(len(weapons), dtype=torch.float).scatter_(dim=0, index=torch.tensor(weapons.index(y)), value=1))
+
+def _tensor_transform(name_lambda, weapon_lambda, data):
+    tensor_accumulator = torch.cat((name_lambda(data[NAME_1_COL_IDX]),
+                                    weapon_lambda(data[WEAPON_1_1_COL_IDX])), 0)
+
+    tensor_accumulator = torch.cat((tensor_accumulator,
+                                    weapon_lambda(data[WEAPON_1_2_COL_IDX])), 0)
+
+    tensor_accumulator = torch.cat((tensor_accumulator,
+                                    name_lambda(data[NAME_2_COL_IDX])), 0)
+
+    tensor_accumulator = torch.cat((tensor_accumulator,
+                                    weapon_lambda(data[WEAPON_2_1_COL_IDX])), 0)
+
+    tensor_accumulator = torch.cat((tensor_accumulator,
+                                    weapon_lambda(data[WEAPON_2_2_COL_IDX])), 0)
+
+    return tensor_accumulator
+
+def get_botdata_features(botdata_path):
+    #Returns bot names, bot weapons, as lists
+    botnames = []
+    weapons = []
+
+    input_data = pd.read_csv(botdata_path, header=None)
+
+    for rowidx in range(0, input_data.shape[0]):
+        #Add both competitor names to the botnames list
+        botnames.append(input_data.iloc[rowidx][NAME_1_COL_IDX])
+        botnames.append(input_data.iloc[rowidx][NAME_2_COL_IDX])
+
+        #Bot 1 weapons
+        weapons.append(input_data.iloc[rowidx][WEAPON_1_1_COL_IDX])
+        weapons.append(input_data.iloc[rowidx][WEAPON_1_2_COL_IDX])
+
+        #Bot 2 weapons
+        weapons.append(input_data.iloc[rowidx][WEAPON_2_1_COL_IDX])
+        weapons.append(input_data.iloc[rowidx][WEAPON_2_2_COL_IDX])
+
+    #Dedupe the lists
+    botnames = list(set(botnames))
+    weapons = list(set(weapons))
+
+    return botnames, weapons
+
+def get_botdata_lambdas(botnames, weapons):
+    #Returns a function for converting botnames to a 1D hot, and weapon to a 1D hot
+    botname_lambda = _get_botname_lambda(botnames)
+    weapon_lambda = _get_weapon_lambda(weapons)
+
+    return botname_lambda, weapon_lambda
+
+def get_tensor_transform(botname_lambda, weapon_lambda):
+    #Returns a function converting data to a 1D tensor
+    return partial(_tensor_transform, botname_lambda, weapon_lambda)
---
+++ bottorch.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+import botdata
+import torch
+
+from torch import nn
+from torch.utils.data import DataLoader
+
+DATA_PATH = 'BotTorch_Data-Training.csv'
+
+DEVICE = "cpu"
+
+#Percentage of samples used for training
+TRAINING_SPLIT_PERCENTAGE = 0.7
+
+#Number of times to do the train / test cycle
+EPOCHS = 5000
+
+class BotdataNeuralNetwork(nn.Module):
+    def __init__(self, botdata_tensor_size, label_tensor_size):
+        super(BotdataNeuralNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(botdata_tensor_size, botdata_tensor_size),
+            nn.ReLU(),
+            nn.Linear(botdata_tensor_size, botdata_tensor_size),
+            nn.ReLU(),
+            nn.Linear(botdata_tensor_size, label_tensor_size)
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+def train(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(DEVICE), y.to(DEVICE)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+def test(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(DEVICE), y.to(DEVICE)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+
+            correct += (pred.argmax(1) == y.argmax(1)).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+def main():
+    bot_names, bot_weapons = botdata.get_botdata_features(DATA_PATH)
+    botname_lambda, weapon_lambda = botdata.get_botdata_lambdas(bot_names, bot_weapons)
+    botdata_transform = botdata.get_tensor_transform(botname_lambda, weapon_lambda)
+
+    botdataset = botdata.BotDataset(DATA_PATH, transform=botdata_transform, target_transform=botname_lambda)
+
+    print(botdataset[0][0].shape)
+
+    print(len(botdataset))
+
+    training_size = int(TRAINING_SPLIT_PERCENTAGE * len(botdataset))
+    test_size = len(botdataset) - training_size
+
+    training_data, _, test_data = torch.utils.data.random_split(botdataset, (training_size, 0, test_size))
+
+    print(len(training_data))
+    print(len(test_data))
+
+    model = BotdataNeuralNetwork(botdataset[0][0].shape[0], len(bot_names)).to(DEVICE)
+
+    print(model)
+
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+    train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
+    test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
+
+    for t in range(EPOCHS):
+        print(f"Epoch {t+1}\n-------------------------------")
+        train(train_dataloader, model, loss_fn, optimizer)
+        test(test_dataloader, model, loss_fn)
+
+    print("Done!")
+
+if __name__ == "__main__":
+    main()
