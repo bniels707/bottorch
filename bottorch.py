dfb361a60bd0f929c078765c3e65e86ec3987e09
Brandon Nielsen <nielsenb@jetfuse.net>
2022-03-06 13:47:27 -0600

    Add crude hyperparameter tuning.

--- bottorch.py
+++ bottorch.py
@@ -16,7 +16,7 @@ TRAINING_SPLIT_PERCENTAGE = 0.7
 BATCH_SIZE = 64
 
 #Number of times to do the train / test cycle
-EPOCHS = 50
+EPOCHS = 5
 
 BINARY_THRESHOLD = torch.tensor([0.5])
 
@@ -62,7 +62,6 @@ def train(dataloader, model, loss_fn, optimizer):
 
         # Print results after each batch
         trained_counter += len(X)
-        print(f"loss: {loss.item():>7f}  [{trained_counter:>5d}/{size:>5d}]")
 
 def test(dataloader, model, loss_fn):
     size = len(dataloader.dataset)
@@ -81,9 +80,57 @@ def test(dataloader, model, loss_fn):
 
             correct += (pred == y).type(torch.float).sum().item()
 
-    test_loss /= num_batches
-    correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    return test_loss, correct
+
+def train_iteration(botdataset, training_size, test_size, epochs, l1, l2):
+    training_data, _, test_data = torch.utils.data.random_split(botdataset, (training_size, 0, test_size))
+
+    model = BotdataNeuralNetwork(botdataset[0][0].shape[0], l1, l2).to(DEVICE)
+
+    loss_fn = nn.BCELoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-1)
+
+    train_dataloader = DataLoader(training_data, batch_size=BATCH_SIZE, shuffle=True)
+    test_dataloader = DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=True)
+
+    for t in range(epochs):
+        print(f"Epoch {t+1}\n-------------------------------")
+        train(train_dataloader, model, loss_fn, optimizer)
+        test_loss, test_correct = test(test_dataloader, model, loss_fn)
+        print(f"Test Error: \n Accuracy: {(100*(test_correct / len(test_dataloader.dataset))):>0.1f}%, Avg loss: {test_loss / len(test_dataloader):>8f} \n")
+
+    return test_loss, test_correct, model
+
+def hyper_tune(botdataset, training_size, test_size, step_size, training_epochs):
+    current_tuning_step = 0
+    tuning_steps = len(range(botdataset[0][0].shape[0] // 10, botdataset[0][0].shape[0] * 10, step_size)) * len(range(botdataset[0][0].shape[0] // 10, botdataset[0][0].shape[0] * 10, step_size))
+
+    best_correct = 0
+    best_l1 = 0
+    best_l2 = 0
+
+    #Parameter tuning loop
+    for test_l1 in range(botdataset[0][0].shape[0] // 10, botdataset[0][0].shape[0] * 10, step_size):
+        for test_l2 in range(botdataset[0][0].shape[0] // 10, botdataset[0][0].shape[0] * 10, step_size):
+            print(f"L1: {test_l1}, L2: {test_l2}")
+
+            test_loss, test_correct, _ = train_iteration(botdataset, training_size, test_size, training_epochs, test_l1, test_l2)
+
+            if test_correct > best_correct:
+                best_correct = test_correct
+                best_l1 = test_l1
+                best_l2 = test_l2
+
+            current_tuning_step += 1
+
+            print(f"Tuning step: {current_tuning_step}/{tuning_steps}")
+
+def tune(botdataset, training_size, test_size, l1, l2, training_epochs):
+    test_loss, test_correct, model = train_iteration(botdataset, training_size, test_size, training_epochs, l1, l2)
+
+    print(f"Done! Accuracy {(100*(test_correct / test_size)):>0.1f}%")
+
+    return model
 
 def main():
     bot_names, bot_weapons = botdata.get_botdata_features(DATA_PATH)
@@ -92,34 +139,11 @@ def main():
 
     botdataset = botdata.BotDataset(DATA_PATH, transform=botdata_transform, target_transform=botdata.get_classification_lambda())
 
-    print(botdataset[0][0].shape)
-
-    print(len(botdataset))
-
     training_size = int(TRAINING_SPLIT_PERCENTAGE * len(botdataset))
     test_size = len(botdataset) - training_size
 
-    training_data, _, test_data = torch.utils.data.random_split(botdataset, (training_size, 0, test_size))
-
-    print(len(training_data))
-    print(len(test_data))
-
-    model = BotdataNeuralNetwork(botdataset[0][0].shape[0], len(bot_names)).to(DEVICE)
-
-    print(model)
-
-    loss_fn = nn.BCELoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=1e-1)
-
-    train_dataloader = DataLoader(training_data, batch_size=BATCH_SIZE, shuffle=True)
-    test_dataloader = DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=True)
-
-    for t in range(EPOCHS):
-        print(f"Epoch {t+1}\n-------------------------------")
-        train(train_dataloader, model, loss_fn, optimizer)
-        test(test_dataloader, model, loss_fn)
-
-    print("Done!")
+    #hyper_tune(botdataset, training_size, test_size, 100, 5) #L1: 32, L2: 1332
+    tune(botdataset, training_size, test_size, 32, 1332, 1000)
 
 if __name__ == "__main__":
     main()
